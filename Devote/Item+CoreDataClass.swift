1674fc5fe6f68f414080aded4d5efd41a7361892
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-13 17:07:14 +0530

    feature: seeting up core data class and managed object

---
+++ Devote/Item+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  Item+CoreDataClass.swift
+//  Devote
+//
+//  Created by Kshitij Singh Chauhan on 13/12/23.
+//
+//
+
+import Foundation
+import CoreData
+
+public class Item: NSManagedObject {
+
+}
