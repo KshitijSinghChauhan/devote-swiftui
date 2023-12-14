00d37eb2e502b5ce4b03fbcb1a41a1341d85acff
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-14 18:27:12 +0530

    feature: added dark mode button and functionality

--- Devote/ContentView.swift
+++ Devote/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
     // MARK: - PROPERTY
     
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     @State var task: String = ""
     @State private var showNewTaskItem: Bool = false
     
@@ -46,6 +47,39 @@ struct ContentView: View {
                 // MARK: - MAIN VIEW
                 VStack {
                     // MARK: - HEADER
+                    
+                    HStack(spacing: 10) {
+                        
+                        // TITLE
+                        Text("Devote")
+                            .font(.system(.largeTitle, design: .rounded, weight: .heavy))
+                            .padding(.leading, 4)
+                        Spacer()
+                        
+                        // EDIT BUTTON
+                        EditButton()
+                            .font(.system(size: 16, weight: .semibold, design: .rounded))
+                            .padding(.horizontal, 10)
+                            .frame(minWidth: 70, minHeight: 24)
+                            .background(
+                                Capsule().stroke(Color.white, lineWidth: 2)
+                            )
+                        
+                        // APPEARANCE BUTTON
+                        Button {
+                            // TOGGLE APPEARANCE
+                            isDarkMode.toggle()
+                        } label: {
+                            Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .font(.system(.title, design: .rounded))
+                        }
+
+                    } //: HSTACK
+                    .padding()
+                    .foregroundColor(.white)
+                    
                     Spacer(minLength: 80)
                     
                     // MARK: - NEW TASK BUTTON
@@ -107,11 +141,7 @@ struct ContentView: View {
             } //: ZSTACK
             .navigationTitle("Daily Task")
             .navigationBarTitleDisplayMode(.large)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-            } //: TOOLBAR
+            .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
             )
