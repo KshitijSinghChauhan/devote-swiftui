353406aea955be6ecf13d2fbedd499e8d67d2cd7
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-15 17:57:34 +0530

    feature: integrated WidgetKit + added widgets

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Kshitij Singh Chauhan on 15/12/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
